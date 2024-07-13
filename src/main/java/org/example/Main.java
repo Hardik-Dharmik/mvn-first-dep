336cc83510a9fc04000783055530ddf88502509c
Hardik <hardikdharmik94@gmail.com>
2024-07-13 18:32:42 +0530

    Initial Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person person = new Person("Hardik", 22);
+        System.out.println(person);
+    }
+}
